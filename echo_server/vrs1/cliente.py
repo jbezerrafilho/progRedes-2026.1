03111c393e41562d06393caf35eae2696b4da914
José Bezerra Filho <jbezerra.filho@outlook.com>
2026-06-06 20:35:39 -0300

    Create folder vrs1

---
+++ echo_server/vrs1/cliente.py
@@ -0,0 +1,15 @@
+import socket
+from config import SERVER, PORT
+
+my_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+while True:
+    msg = input('Mensagem: ').encode()
+    if msg:
+        print(f"Enviando: {msg}")
+        my_sock.sendto(msg, (SERVER, PORT))
+        answer, source = my_sock.recvfrom(512)
+        print(f"Recebido de {source}: {answer}")
+    else:
+        break
+my_sock.close()
