018229f6fb5a6e130abccf161ab684f8d9bdafc3
= <=>
2026-06-21 20:38:09 -0300

    change client.py

--- tcp/echo_server/client.py
+++ tcp/echo_server/client.py
@@ -1,7 +1,7 @@
 # Echo client program
 import socket
 
-HOST = '192.168.20.133'    # The remote host
+HOST = '192.168.20.133'   # The remote host
 PORT = 50007              # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
